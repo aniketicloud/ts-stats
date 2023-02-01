9bf62d769c525cbd94cec29ddced48c31fda9b47
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-02-01 19:55:48 +0100

    converting row values

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { dateStringToDate } from "./utils";
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -10,6 +11,15 @@ export class CsvFileReader {
         encoding: "utf-8",
       })
       .split("\n")
-      .map((row: string) => row.split(","));
+      .map((row: string): string[] => row.split(","))
+      .map((row: string[]): any => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+        ];
+      });
   }
 }
