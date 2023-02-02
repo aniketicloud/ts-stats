0067745b7a6b9e088f79aa3aabd58c0cf905e14b
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-02-02 02:27:01 +0100

    describing a row with a tuple

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,8 +1,13 @@
 import fs from "fs";
+import { MatchResult } from "./MatchResult";
 import { dateStringToDate } from "./utils";
 
+// tuple for one row data
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-  data: string[][] = [];
+  // data is 2d array [][]
+  data: MatchData[] = [];
   constructor(public filename: string) {}
 
   read(): void {
@@ -12,13 +17,15 @@ export class CsvFileReader {
       })
       .split("\n")
       .map((row: string): string[] => row.split(","))
-      .map((row: string[]): any => {
+      .map((row: string[]): MatchData => {
         return [
           dateStringToDate(row[0]),
           row[1],
           row[2],
           parseInt(row[3]),
           parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6],
         ];
       });
   }
