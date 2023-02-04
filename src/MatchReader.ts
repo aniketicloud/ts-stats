d7e3902c363971215041910e523697d5d54ab986
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-02-04 22:26:28 +0530

    Transforming Data

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,4 +1,5 @@
 import { MatchResult } from "./MatchResult";
+import { dateStringToDate } from "./utils";
 
 // tuple for one row data
 type MatchData = [Date, string, string, number, number, MatchResult, string];
@@ -9,17 +10,21 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
 
-  // .map((row: string[]): MatchData => {
-  //   return [
-  //     dateStringToDate(row[0]),
-  //     row[1],
-  //     row[2],
-  //     parseInt(row[3]),
-  //     parseInt(row[4]),
-  //     row[5] as MatchResult,
-  //     row[6],
-  //   ];
-  // })
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6],
+      ];
+    });
+  }
 }
