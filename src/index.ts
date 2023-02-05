43d2624360fcf285a6f9ce773db689495272d26e
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-02-05 17:14:43 +0530

    Static method for preconfigured output

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { HtmlReport } from "./reportTargets/HtmlReport";
 import { Summary } from "./Summary";
 
@@ -13,9 +12,6 @@ const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new HtmlReport("ManUnitedSummary")
-);
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
 summary.buildAndPrintReport(matchReader.matches);
